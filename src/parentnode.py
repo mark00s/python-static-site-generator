aaa79f838e910d1a2aa22090075e13169ae71d59
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-03-30 21:01:50 +0200

    feat: ParentNode

---
+++ src/parentnode.py
@@ -0,0 +1,29 @@
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Tag value is missing in ParentNode")
+        
+        if not self.children:
+            raise ValueError("Children value is missing in ParentNode")
+
+
+        html = f"<{self.tag}>"
+        if isinstance(self, LeafNode):
+            return self.to_html()
+        
+        for child in self.children:
+            html += child.to_html()
+
+        html += f"</{self.tag}>"
+
+        return html
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
