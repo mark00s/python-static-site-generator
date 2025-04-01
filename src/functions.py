9ec6b7b74cd0c895bd0d5a75f2502257e1ccdb04
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-04-01 22:37:28 +0200

    feat: split_nodes_delimiter

--- src/functions.py
+++ src/functions.py
@@ -18,3 +18,26 @@ def text_node_to_html_node(text_node: TextNode):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError(f"invalid text type: {text_node.text_type}")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Markdown formatting error. Section not closed")
+
+        for i, section in enumerate(sections):
+            # Last one can be empty ("")
+            if section:
+                if i % 2 == 0:
+                    # Even, then around delimeters
+                    new_nodes.append(TextNode(section, TextType.TEXT))
+                else:
+                    # Odd, then inside delimeters
+                    new_nodes.append(TextNode(section, text_type))
+    return new_nodes
